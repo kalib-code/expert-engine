a15a584b8b0dbb7df8bca2a7764528f86a11960c
Karl Kenneth Alibuas <karl.kenneth@singlife.com>
2022-05-22 11:11:22 +0800

    add response 200 to root

--- routes/root.js
+++ routes/root.js
@@ -2,6 +2,7 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return { root: true }
+    reply.code(200).send({ statusCode: '200',
+      message: 'Welcome to the API' })
   })
 }
